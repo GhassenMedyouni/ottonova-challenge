f8c1ae7dc6c9e2db3544df2d93370e42b2ebf7e5
Ghassen <ghassen.medyouni@gmail.com>
2024-04-23 00:37:50 +0200

    add city interface

---
+++ backend/src/cities/city.interface.ts
@@ -0,0 +1,10 @@
+export interface City {
+    name: string;
+    name_native: string;
+    country: string;
+    continent: string;
+    latitude: string;
+    longitude: string;
+    founded: string;
+    landmarks: string[];
+}
