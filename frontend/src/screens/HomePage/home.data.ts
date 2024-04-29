043296181074ff5c2686393e556427f91e13f861
Ghassen <ghassen.medyouni@gmail.com>
2024-04-29 04:19:58 +0200

    add home page

---
+++ frontend/src/screens/HomePage/home.data.ts
@@ -0,0 +1,27 @@
+export const mainTitle = "Discover the World’s Hidden Gems"
+export const mainDescription = "Welcome to our city explorer platform, where adventure awaits at every corner\u00a0!\u00a0🌎✨ " +
+    "Whether you’re a curious traveler, an urban enthusiast, or simply someone seeking new experiences, we’ve got you covered." +
+    " Dive into the heart of vibrant cities, uncover their unique stories, and immerse yourself in local culture."
+
+export const valuesTitle = "What We Offer"
+export const valuesDescription = "Join us on this captivating journey as we celebrate the diversity of cities worldwide. Adventure awaits—let’s explore together! 🗺️🏙️";
+export const values = [
+    {
+        name: 'City Guides',
+        description:
+            'Explore meticulously curated guides for each city, featuring must-visit landmarks, off-the-beaten-path gems, and insider tips.'
+    },
+    {
+        name: 'Data Insights',
+        description:
+            'Delve into fascinating data about population dynamics, historical events, and urban development. Discover how cities evolve over time.'
+    },
+    {
+        name: 'Interactive Maps',
+        description:
+            'Navigate effortlessly through bustling streets, scenic parks, and iconic neighborhoods using our interactive maps.'
+    },
+]
+export const citiesSectionTitle = "Explore Our Cities";
+export const citiesSectionDescription = "Discover the charm and diversity of our cities. " +
+    "From bustling metropolises to quaint towns, each destination offers a unique experience waiting to be explored."
